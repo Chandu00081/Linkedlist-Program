e51c5e078cf5574e87cca564d0f6587c3db272f8
chandu00081 <chandu00081@gmail.com>
2023-02-12 21:21:13 +0530

    creating linkedlist

---
+++ AddList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkldist
+{
+    internal class AddList
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LinkedList Program");
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(32);
+            linkedList.Add(54);
+            linkedList.Add(76);
+            linkedList.Add(211);
+            linkedList.Display();
+
+
+        }
+    }
+}
