986a55d47eb88826cc8da625d013d74bcf666079
chandu00081 <chandu00081@gmail.com>
2023-02-14 20:19:58 +0530

    appending

---
+++ AddList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkldist
+{
+    internal class AddList
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList linkedList = new LinkedList();
+
+            Console.WriteLine("Add elements into Linkedlist using AddLast Method:");
+            linkedList.Add(23);
+            linkedList.Add(92);
+            linkedList.Add(12);
+            linkedList.Display();
+
+
+
+        }
+    }
+}
