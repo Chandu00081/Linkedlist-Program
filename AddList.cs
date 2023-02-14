118dccabafdb13e3eac105d2cad3a28839501ada
chandu00081 <chandu00081@gmail.com>
2023-02-14 20:30:56 +0530

    Insertion

---
+++ AddList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkldist
+{
+    internal class List
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Welcome to the LinkedList Program");
+
+                LinkedList linkdedList = new LinkedList();
+                linkdedList.Add(45);
+                linkdedList.Add(32);
+                linkdedList.InsertAtParticularPosition(2, 56);
+                linkdedList.Display();
+            }
+
+
+
+
+        }
+    }
+}
